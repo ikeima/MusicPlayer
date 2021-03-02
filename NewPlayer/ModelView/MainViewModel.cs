8a5d2ee5b014be72039401df21d6989012a4f7f1
ikeima <ikeima@yandex.ru>
2021-03-02 16:58:23 +0500

    added main function of the player

--- NewPlayer/ModelView/MainViewModel.cs
+++ NewPlayer/ModelView/MainViewModel.cs
@@ -1,12 +1,17 @@
 ﻿using NewPlayer.Model;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Windows.Forms;
+using System.Windows.Media;
 
 namespace NewPlayer.ModelView
 {
     class MainViewModel : BaseViewModel
     {
+        private bool _isResume = false;
+
         private ObservableCollection<string> _musicCollection = new ObservableCollection<string>();
         public ObservableCollection<string> MusicCollection
         {
@@ -15,6 +20,49 @@ namespace NewPlayer.ModelView
             {
                 _musicCollection = value;
                 OnPropertyChanged("MusicCollection");
+            }
+        }
+
+        private string _selectedSong;
+        public string SelectedSong
+        {
+            get { return _selectedSong; }
+            set
+            {
+                _selectedSong = value;
+
+                foreach (string song in MusicsPath)
+                {
+                    if (song.Contains(_selectedSong))
+                    {
+                        SelectedSongPath = song;
+                        break;
+                    }
+                }
+
+                OnPropertyChanged("SelectedSong");
+            }
+        }
+
+        private string _selectedSongPath;
+        public string SelectedSongPath
+        {
+            get { return _selectedSongPath; }
+            set
+            {
+                _selectedSongPath = value;
+                OnPropertyChanged("SelectedSongPath");
+            }
+        }
+
+        private List<string> _musicsPath = new List<string>();
+        public List<string> MusicsPath
+        {
+            get { return _musicsPath; }
+            set
+            {
+                _musicsPath = value;
+                OnPropertyChanged("MusicsPath");
             }
         }
 
@@ -36,14 +84,111 @@ namespace NewPlayer.ModelView
                                 if (Path.GetExtension(file.FullName) == ".mp3")
                                 {
                                     MusicCollection.Add(file.Name);
+                                    MusicsPath.Add(file.FullName);
                                 }
                             }
                         }
                     }
                 });
-
                 return _loadMusic;
             }
         }
+
+        private MediaPlayer _player = new MediaPlayer();
+        public MediaPlayer Player
+        {
+            get { return _player; }
+            set
+            {
+                _player = value;
+                OnPropertyChanged("Player");
+            }
+        }
+
+
+
+        private RelayCommand _play;
+        public RelayCommand Play
+        {
+            get
+            {
+                _play = new RelayCommand(obj =>
+                {
+                    Player.Open(new Uri(SelectedSongPath));
+                    Player.Play();
+                    _isResume = true;
+                });
+                return _play;
+            }
+        }
+
+        private RelayCommand _pause;
+        public RelayCommand Pause
+        {
+            get
+            {
+                _pause = new RelayCommand(obj =>
+                {
+                    Player.Pause();
+                });
+                return _pause;
+            }
+        }
+
+        private RelayCommand _resume;
+        public RelayCommand Resume
+        {
+            get
+            {
+                _resume = new RelayCommand(obj =>
+                {
+                    Player.Play();
+                });
+                return _resume;
+            }
+        }
+
+
+        private RelayCommand _next;
+        public RelayCommand Next
+        {
+            get
+            {
+                _next = new RelayCommand(obj =>
+                {
+                    int indexNext = MusicsPath.IndexOf(SelectedSongPath) + 1;
+                    Player.Open(new Uri(MusicsPath[indexNext]));
+                    SelectedSong = MusicsPath[indexNext];
+                    Player.Play();
+                });
+                return _next;
+            }
+        }
+
+        private RelayCommand _previous;
+        public RelayCommand Previous
+        {
+            get
+            {
+                _previous = new RelayCommand(obj =>
+                {
+                    try
+                    {
+                        int indexPrevious = MusicsPath.IndexOf(SelectedSongPath) - 1;
+                        Player.Open(new Uri(MusicsPath[indexPrevious]));
+                        SelectedSong = MusicsPath[indexPrevious];
+                        Player.Play();
+                    }
+                    catch
+                    {
+                        Player.Open(new Uri(SelectedSongPath));
+                        Player.Play();
+                    }
+              
+                });
+                return _previous;
+            }
+        }
+
     }
 }
